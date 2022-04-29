329cbeadc024bba6d928c7b0644c23f6ae84f746
aloprui26-06 <102860876+aloprui26-06@users.noreply.github.com>
2022-04-29 17:28:30 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,18 @@
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    `)
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+basic.pause(2000)
 basic.clearScreen()
 basic.forever(function () {
-    maqueen.servoRun(maqueen.Servos.S1, 41)
+    maqueen.servoRun(maqueen.Servos.S1, 100)
     basic.pause(100)
-    maqueen.servoRun(maqueen.Servos.S1, 38)
+    maqueen.servoRun(maqueen.Servos.S1, 140)
     basic.pause(100)
-    maqueen.servoRun(maqueen.Servos.S1, 68)
+    maqueen.servoRun(maqueen.Servos.S1, 180)
     basic.pause(100)
 })
